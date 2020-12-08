84d3d273d0cf628374ecc2e90e6914e2ac5d7cb5
Tapan <tapankarnik@gmail.com>
2020-12-08 21:34:37 +0000

    Added exception handling

--- app.py
+++ app.py
@@ -17,6 +17,17 @@ def gen_jobs():
     """
     if request.is_json:
         data = request.get_json()
+        try:
+            if len(data['job_name'])>20 or len(data['job_name'])==0 or data['job_name'] == None:
+                raise Exception('Invalid Job Name')
+            if data['num_jobs']<0 or data['num_jobs']==0 or data['num_jobs']>100:
+                raise Exception('Invalid Number of Jobs')
+            if data['job_duration']<=0 or data['job_duration']>30:
+                raise Exception('Invalid Job Duration')
+
+        except Exception:
+            return "Invalid Input", 400
+
         job_name = data['job_name']
         num_jobs = data['num_jobs']
         job_duration = data['job_duration']
@@ -25,8 +36,10 @@ def gen_jobs():
             # Sends POST Requests to the DCN one after the other.
 
             payload = {'job_name':job_name, 'job_id':job_id, 'total_jobs':num_jobs, 'job_duration':job_duration}
-            r = requests.post('https://www.google.com',params=payload) # POST Request -> IP Addr of the DCN
+            url = "localhost:5000" # IP for the DCN
+            r = requests.post(url, params=payload, content_type='application/json') # POST Request -> IP Addr of the DCN
         return b"OK", 200
+
     else:
         return b"Not a JSON input", 400
 
